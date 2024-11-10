a1d69b503f298502e15eb29f820100931246786e
Sheriff Aderombi <ayaderombi@gmail.com>
2024-11-10 10:09:07 +0100

    progress

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,9 +4,10 @@ import React from 'react';
 const About = () => {
     return ( 
         <>
-        <div>
-            <h2>about page</h2>
-        </div>
+        <section className="section about-section">
+            <h1 className="section-title">about us</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et iste consequatur aliquam commodi unde reiciendis itaque rerum est placeat quisquam dignissimos iusto cumque magnam rem asperiores, quae ipsa nostrum sunt. Veniam deserunt laborum possimus eligendi nostrum ullam commodi earum magnam?</p>
+        </section>
         </>
      );
 }
